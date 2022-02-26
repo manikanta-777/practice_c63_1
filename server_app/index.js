a89b8b44e335ae7fc8d44206fa331b4d0b45a103
chintakayala manikantha <2078320@cognizant.com>
2022-02-26 12:55:58 +0530

    hihi request path
    added hihi request path

--- server_app/index.js
+++ server_app/index.js
@@ -6,12 +6,14 @@ app.get("/", function (req, res) {
     res.send("Welcome to my home page");
 })
 app.get('/hello', function (req, res) {
-    res.send("Hello Page");
+    res.send("you are in Hello Page");
 })
 app.get('/hi', function (req, res) {
-    res.send("Hi Page");
+    res.send("you are in Hi Page ");
 })
-
+app.get('/hihi', function (req, res) {
+    res.send("you are in Hihi Page ");
+})
 app.listen(5005, function (err) {
     if (err) {
         console.log("Error occured while server startup");
